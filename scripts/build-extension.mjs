da34e275f33527043f6c0512cbffea8e3621ff68
kuni <k.k4m8n11t9@gmail.com>
2026-06-23 19:46:24 +0900

    fix: ホカンテの背景透過済みラベルからフラスコアイコンを削除

--- scripts/build-extension.mjs
+++ scripts/build-extension.mjs
@@ -21,7 +21,7 @@ await build({
 });
 
 await Promise.all(
-  ["manifest.json", "popup.html", "popup.css", "icon-16.png", "icon-48.png", "icon-128.png"].map((fileName) =>
+  ["manifest.json", "popup.html", "popup.css", "icon-16.png", "icon-48.png", "icon-128.png", "map-rpg.svg"].map((fileName) =>
     copyFile(
       resolve(extensionRoot, "public", fileName),
       resolve(outputDirectory, fileName),
